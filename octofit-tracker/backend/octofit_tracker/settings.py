05b0e5b375a81dde3ec79d2487b4c12b07944009
vparaschiverente <v.paraschiv-terente@accenture.com>
2025-11-10 08:44:29 +0000

    .

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-6c(e*2q%2ugp9l37k*)$3fo#r&#$t(9)wk_aa4vr#^+^jx+_8u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}.github.dev')
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -14,8 +14,74 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from octofit_tracker import models
+from rest_framework import viewsets, serializers
+
+# Serializers
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Team
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Activity
+        fields = '__all__'
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Workout
+        fields = '__all__'
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Leaderboard
+        fields = '__all__'
+
+# ViewSets
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = models.Team.objects.all()
+    serializer_class = TeamSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = models.User.objects.all()
+    serializer_class = UserSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = models.Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = models.Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+class LeaderboardViewSet(viewsets.ModelViewSet):
+    queryset = models.Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+
+# Routers
+router = routers.DefaultRouter()
+router.register(r'teams', TeamViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'workouts', WorkoutViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
+# API endpoints will be available at:
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/activities/
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/users/
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/teams/
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/workouts/
+#   https://$CODESPACE_NAME-8000.app.github.dev/api/leaderboard/
